d667b3b452e2115f64da39c35132b56ebe2c6d91
Yudi Muttaqin <92357306+yudimasbruh@users.noreply.github.com>
2023-03-17 14:20:59 +0700

    update foto dan file checkout

--- resources/views/main/checkout.blade.php
+++ resources/views/main/checkout.blade.php
@@ -7,7 +7,7 @@
             </div>
             <div class="col-md-6">
                 <h4 class="fw-bold">{{ $data->nama }}</h4>
-                <p>Ketarangan baju batik</p>
+                <p>Ketarangan baju muslim laki-laki yang biasa dipakai untuk sholat yaitu gamis kini tersedia dengan berbagai warna</p>
                 <h3>Rp. {{ $data->harga }}</h3>
 
                 <a href="https://wa.me/0895704919630/?text=saya ingin membeli barang {{ $data->nama }}" target="_blank" class="btn btn-primary btn-sm mt-3">Beli Sekarang</a>
